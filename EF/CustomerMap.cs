5c86f42999bbc13173a78fc311a4d17f553b7f0e
Srinivas Male (MINDTREE LIMITED) <maalesreenivas@gmail.com>
2019-08-27 17:36:19 +0530

    Add project files.

---
+++ EF/CustomerMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace CoreWebAppl
+{
+    public class CustomerMap
+    {
+        public CustomerMap(EntityTypeBuilder<Customer> entityBuilder)
+        {
+            entityBuilder.HasKey(t => t.Id);
+            entityBuilder.Property(t => t.FirstName).IsRequired();
+            entityBuilder.Property(t => t.LastName).IsRequired();
+            entityBuilder.Property(t => t.Email).IsRequired();
+            entityBuilder.Property(t => t.MobileNo).IsRequired();
+        }
+    }
+}
---
+++ Migrations/20190720145112_sampldb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreWebAppl.Migrations
+{
+    public partial class sampldb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AddedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false),
+                    IPAddress = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    MobileNo = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
